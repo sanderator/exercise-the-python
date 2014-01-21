8bbda20d1b48a05e2206fa932c5069427c56c0e4
Peter SANDER <sander@polytech.unice.fr>
2014-01-21 13:53:45 +0100

    Initial todo.

---
+++ exo0/code/src/letters/letter_count.py
@@ -0,0 +1,11 @@
+def letter_count(strng, letter):
+    '''
+    Counts the number of times a given letter appears in a string.
+
+    @param strng: A string.
+    @param letter: The letter to count in the string.
+    @return: The number of occurrences of the letter.
+
+    @author:
+    '''
+    pass
---
+++ exo0/code/src/letters/list_letter_count.py
@@ -0,0 +1,14 @@
+import # something
+
+
+def list_letter_count(lst, letter):
+    '''
+    Counts the number of times a given letter appears in the strings of a list.
+    
+    @param strng: A list whose elements are strings.
+    @param letter: The letter to count in the strings.
+    @return: The number of occurrences of the letter.
+
+    @author:
+    '''
+    pass
