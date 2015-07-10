cb1a26b37370c181c106709fdc2f5328368d3c8f
Peter Sander <sander@unice.fr>
2015-07-10 09:57:41 +0200

    Fixed some syntax.

--- ds0/one.py
+++ ds0/one.py
@@ -1,7 +1,7 @@
-ph=input("une phrase") #1
+fichier = input("une phrase") #1
 
-i=0 #2
+i = 0 #2
 
-while i<len(ph) and ph[i] !=" " : #3
-    print(ph[i],end="") #4
-    i = i+1 #5
+while i < len(fichier) and fichier[i] != " " : #3
+    print(fichier[i], end="") #4
+    i += 1 #5
--- ds0/two.py
+++ ds0/two.py
@@ -1,10 +1,10 @@
-ph=input("une phrase: ") #1
+fichier = input("une phrase: ") #1
 lettre = input("une lettre: ")
 
-i=0 #2
+i = 0 #2
 
-for l in ph: #3
+for l in fichier: #3
     if l == lettre:
         i += 1
+        
 print('%i occurrences de %s' % (i, lettre))
-
